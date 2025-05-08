e9b3cfd1297d5314054dfb8860fd672219626ef0
jsrinth <164851400+jsrinth@users.noreply.github.com>
2025-05-08 10:16:53 +0800

    adding invoice and pagination

---
+++ .next/static/chunks/app_dashboard_invoices_page_tsx_8bcfde._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_dashboard_invoices_page_tsx_8bcfde._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_dashboard_invoices_page_tsx_8bcfde._.js",
+  "chunks": [
+    "static/chunks/node_modules__pnpm_9eb612._.js",
+    "static/chunks/app_fbd2c2._.js"
+  ],
+  "source": "dynamic"
+});
